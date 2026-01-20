e69eeddcd2dfefc3a107bfd82198ac2509df6585
sandeep-core <sandeepbharanghar@gmail.com>
2026-01-20 12:49:54 +0530

    connection stablised

---
+++ src/main/java/com/example/day3/model/StudentModel.java
@@ -0,0 +1,19 @@
+package com.example.day3.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.lang.annotation.Documented;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
+public class StudentModel {
+    private String id;
+    private String name;
+    private int age;
+    private String email;
+}
